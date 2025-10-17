bde7721abfc632964222faf68c97020aa9310c05
notlelouch <notlelouchvb@gmail.com>
2025-10-17 23:32:00 +0530

    instructions not implemented correctly

--- anchor-escrow-starter-q4-25/programs/anchor-escrow-q4-25/src/instructions/take.rs
+++ anchor-escrow-starter-q4-25/programs/anchor-escrow-q4-25/src/instructions/take.rs
@@ -12,10 +12,59 @@ use anchor_spl::{
 
 use crate::Escrow;
 
-// #[derive(Accounts)]
-// pub struct Take<'info> {
-//      TODO: Implement Take Accounts
-// }
+#[derive(Accounts)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account(
+        mint::token_program = token_program
+        )]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mint::token_program = token_program
+        )]
+    pub mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program
+        )]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+    init_if_needed,
+    payer = taker,
+    associated_token::mint = mint_b,
+    associated_token::authority = maker,
+    associated_token::token_program = token_program
+)]
+    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        bump
+        )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"escrow", maker.key().as_ref(), seed.to_le_bytes().as_ref()],
+        bump
+        )]
+    pub escrow: Account<'info, Escrow>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
 
 // impl<'info> Take<'info> {
 //      TODO: Implement Take Instruction
